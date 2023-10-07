7f2e4d2f1997c68d66ab4402dd332cce2695b61c
justynalodzinska <justyna.lodzinska@smartive.app>
2023-10-07 17:26:47 +0200

    project setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>starting a new project</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ tests/App.test.js
@@ -0,0 +1,7 @@
+import { describe, expect } from "vitest";
+
+describe("App", () => {
+  it("first test", () => {
+    expect(0).toBe(0);
+  })
+})

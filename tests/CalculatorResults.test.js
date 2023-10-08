6b74bf82b8b94688e7c65f853ddc868b45e22e73
justynalodzinska <justyna.lodzinska@smartive.app>
2023-10-08 12:59:52 +0200

    CalculatorResults tests

--- tests/CalculatorResults.test.js
+++ tests/CalculatorResults.test.js
@@ -1,14 +1,27 @@
-import { describe, expect } from "vitest";
+import { describe, it } from "vitest";
+import { mount } from "@vue/test-utils";
+import CalculatorResults from "../src/components/CalculatorResults.vue";
 
 describe("CalculatorResults", () => {
   it("Displays '--' if no props", () => {
+    const wrapper = mount(CalculatorResults, {
+      props: {
+        days: null,
+        months: null,
+        years: null
+      }
+    });
+    expect(wrapper.text()).toContain("-- years-- months-- days");
   })
 
   it("Displays proper data when props are passed", () => {
-
-  })
-
-  it ("Displays '--' when prop change", () => {
-
+    const wrapper = mount(CalculatorResults, {
+      props: {
+        days: 12,
+        months: 3,
+        years: 8
+      }
+    });
+    expect(wrapper.text()).toContain("8 years3 months12 days");
   })
 })
